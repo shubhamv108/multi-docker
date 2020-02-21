825a260d6607a6579e05bcf4dee2b6c8e133b733
Shubham <shubhamvarshney@outlook.com>
2020-02-21 19:36:38 +0530

    af

--- worker/index.js
+++ worker/index.js
@@ -2,26 +2,18 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 10000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-const subscriber = redisClient.duplicate();
-
-if (null == subscriber) {
-    console.log('could not crete subscriber redis');
-}
+const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index - 1) + fib(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-subscriber.on('message', (channel, message) => {
-    console.log('setting index fib in redis set values for index', message, "on channel " + channel);
-    redisClient.hset('values', message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
-subscriber.subscribe('insert');
-
-console.log("Started worker")
+sub.subscribe('insert');
